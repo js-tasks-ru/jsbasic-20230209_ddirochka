913fd4f33dd72d68220ddd9c3187d568907144b5
Dmitriy Dirochka <dmitriy.dirochka@gmail.com>
2023-02-16 20:31:21 +0300

    added solution of 2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,17 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0; // Cумма зарплаты всей команды
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key])) {
+      result += salaries[key];
+    }
+  }
+  return result;
 }
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,15 @@
 let calculator = {
-  // ваш код
+  read: function(a, b) {
+    this.a = a;
+    this.b = b;
+    return a, b;
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
