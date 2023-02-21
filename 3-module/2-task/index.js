c090afa0e44c4700bba1b5d9d80ead2a038c2f02
Dmitriy Dirochka <dmitriy.dirochka@gmail.com>
2023-02-21 19:40:03 +0300

    added solutions of 3-module 2-task and 3-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,13 @@
+let arr = [5, 3, 8, 1];
+let filtered = [];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+    for (let i = 0; i < arr.length; i++) {
+        if (( a < arr[i] && arr[i] < b ) || ( a > arr[i] && arr[i] > b ))  filtered.push(arr[i]);
+        if (a == arr[i] || b == arr[i]) filtered.push(arr[i]);
+    }
+    return filtered;
 }
+
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr ); // [5,3,8,1] (без изменений)
