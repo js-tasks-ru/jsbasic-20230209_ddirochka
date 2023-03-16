4fa9ae72084f35757f5c3c4db16f75eb7c3102c0
Dmitriy Dirochka <dmitriy.dirochka@gmail.com>
2023-03-16 19:04:10 +0300

    added solution of 5-module 3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  const left = document.querySelector('.carousel__arrow.carousel__arrow_left');
+  const right = document.querySelector('.carousel__arrow.carousel__arrow_right');
+
+  const slider = document.querySelector('.carousel__inner');
+  const images = document.querySelectorAll('.carousel__slide .carousel__img');
+
+  let counter = 0;
+  const step = images[0].clientWidth;
+
+  slider.classList.add('transformAnimation')
+
+  left.style.display = 'none'
+
+  right.addEventListener('click', function() { 
+    counter++
+    left.style.display = ''
+
+    if (counter >= images.length - 1) {
+      right.style.display = 'none'
+    } 
+     
+    slider.style.transform = `translateX(-${step * counter}px)`
+  })
+  
+  left.addEventListener('click', function() {
+    counter--
+    right.style.display = ''
+
+    if (counter <= 0) {
+      left.style.display = 'none'
+    } 
+    
+    slider.style.transform = `translateX(-${step * counter}px)`
+  })
+  
 }
